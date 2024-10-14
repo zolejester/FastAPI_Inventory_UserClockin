57402b52ad52828936cef6b3930b7c06d2386bae
zolejester <zole.jester@gmail.com>
2024-10-14 19:07:23 +0530

    New Update

--- config/database.py
+++ config/database.py
@@ -5,7 +5,7 @@ import os
 mongodb_user = os.getenv("MONGODB_USER")
 mongodb_password = os.getenv("MONGODB_PASSWORD")
 
-client = MongoClient("mongodb+srv://f{mongodb_user}:f{mongodb_password}@cluster0.ykzion4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://zolejester:cTBYZSck4InuJefF@cluster0.ykzion4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # MongoDB
 db = client.v1_database
